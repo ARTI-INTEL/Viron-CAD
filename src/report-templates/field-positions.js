43237caed7095bc67cc8c3d4deb8e8d7fcc60b2d
Muhammad Faiq Imran <faiqimran.fi@gmail.com>
2026-07-06 22:39:00 +0400

    Stuff

--- src/report-templates/field-positions.js
+++ src/report-templates/field-positions.js
@@ -20,7 +20,7 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
-const TEMPLATES_DIR = path.resolve(__dirname, '..', 'report-temps');
+const TEMPLATES_DIR = path.resolve(__dirname, 'templates');
 
 /**
  * @typedef {Object} FieldDef
